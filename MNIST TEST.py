b1ab1fa61cd08236932e35d1eb57f2cd73a79e52
Noah <stitgenn1808@my.uwstout.edu>
2025-09-26 13:02:45 -0500

    Revert "Imported and downloaded MNIST Dataset"
    This reverts commit fe399b3349db6696a59544d9e5005c1b2a7f50e9.

--- MNIST TEST.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import torch
-import torchvision
-from torchvision import datasets, transforms
-
-mnist = datasets.MNIST(root='./data', train=False, download=True, transform=None)
