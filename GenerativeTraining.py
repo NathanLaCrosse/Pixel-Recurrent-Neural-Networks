7ba7d4e9c4f7ac13e2301e4fde4af5a103cbe729
Nathan L <mancool5672@gmail.com>
2025-10-16 22:13:23 -0500

    Added (another) model architecture that allows for conditional pixel generation, leading to a model that can features a stronger dependence on previous data. This is essentially an upgraded version of the original TwoDimensionalGRU with all of the newer techniques included.

---
+++ GenerativeTraining.py
@@ -0,0 +1,41 @@
+import os
+import torch
+from torch import nn
+import pandas as pd
+import tqdm as tqdm
+from torch.nn.functional import embedding
+from torch.utils.data import Dataset, DataLoader
+import MNISTData as md
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import numpy as np
+from NetworkArchitecture import RowRNN, OmniRowRNN, GenerativeRowRNN
+
+epochs = 10
+batch_size = 4
+save = "Models/Generative.pt"
+
+dat = md.PixelDataset(color=True, filepath="Datasets/Cartoons/Train")
+
+net = GenerativeRowRNN(embed_size=64, hidden_size=64, num_layers=3)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+for epoch in range(epochs):
+    dat_loader = DataLoader(dat, batch_size=batch_size)
+    progress_bar = tqdm.tqdm(dat_loader, desc=f"Epoch {epoch + 1}/{epochs}")
+
+    for _, batch in enumerate(progress_bar):
+        ims = batch
+
+        optimizer.zero_grad()
+        logits = net(ims)
+
+        loss = loss_fn(logits.reshape(-1, 257), ims[:, :, 1:, 1:].reshape(-1))
+        loss.backward()
+        nn.utils.clip_grad_norm_(net.parameters(), max_norm=1.0)  # Gradient clipping
+        optimizer.step()
+
+        progress_bar.set_postfix({"Loss:": loss.item()})
+
+    torch.save(net.state_dict(), save)
