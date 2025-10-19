5ad04ce23e849e12db4f4c59829c42fa219f7eb0
Nathan L <mancool5672@gmail.com>
2025-10-18 21:12:41 -0500

    Added some commenting and clean up some code. Additionally, a better GUI was set up to visualize differences with "temperature".

--- FaceInfill.py
+++ FaceInfill.py
@@ -9,27 +9,31 @@ import Data as md
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import numpy as np
-from NetworkArchitecture import RowRNN, OmniRowRNN, ConditionalRowRNN
+from NetworkArchitecture import ConditionalRowRNN
 
 
-class MNISTImages(Dataset):
-    def __init__(self,filepath="Datasets/mnist_train.csv", samples=60000):
-        _, self.raw_images = md.load_mnist(filepath=filepath, samples=samples)
-
-    def __len__(self):
-        return len(self.raw_images)
-
-    def __getitem__(self, item):
-        im = torch.tensor(self.raw_images[item], dtype=torch.long)
-        start_of_col = torch.ones((28, 1), dtype=torch.long) * 256
-        im = torch.cat((start_of_col, im), dim=1)
-        return im.view(1, 28, 29)
-
-
-
-
-# ---------- Training Code ----------
 def train_model(training_args):
+    """
+    Trains an infill network to infill randomly removed parts of an image, which are labeled
+    with the intensity 257. Saves to a pt file every epoch.
+
+    :param training_args: A dictionary containing the different settings of the trainer, which are as follows:
+        - epochs: Number of epochs
+        - im_rows: Number of rows in the image. Images are assumed to be square
+        - net: The neural network to train
+        - dat: The dataset object
+        - file_name: The file to save the weights to
+        - device: The device to train on
+        - starting_infill_pixels: The starting count of pixels to convert to an infill token
+        - infill_pixel_increment: The amount of infill pixels increases at this rate every epoch
+        - start_infill_grid: The initial size of an infill pixel
+        - max_infill_grid: The maximum size of an infill pixel
+        - epochs_per_grid_increment: The number of epochs to increment infill grid size
+        - max_infill_pixels: The maximum number of pixels to infill
+        - infill_level_probs: The probability to infill over the RGB, GB and B channels respectively
+    :return:
+    """
+
     net = training_args["net"]
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
@@ -70,7 +74,7 @@ def train_model(training_args):
             else:
                 logits = net(obstructed)
 
-            # Clip out start-of-sequence blip
+            # Clip out start-of-sequence blip - not needed for grading
             logits = logits[:, :, 1:, 1:, :]
             ims = ims[:, :, 1:, 1:]
 
@@ -91,13 +95,64 @@ def train_model(training_args):
         torch.save(net.state_dict(), training_args["file_name"])
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def perform_infill_with_temperature(net, im : torch.Tensor, mask : np.ndarray,
+                                    temp_pixels=10, start_temp=0.8, temp_dec_rate=0.05, min_temp=0.1):
+    """
+
+    :param net:
+    :param im:
+    :param mask:
+    :param temp_pixels:
+    :param start_temp:
+    :param temp_dec_rate:
+    :param min_temp:
+    :return:
+    """
+
+    with torch.no_grad():
+        temp = start_temp
+        reconstructed = torch.clone(im.view(1, 3, mask.shape[0] + 1, mask.shape[1] + 1))
+
+        painted_pixels = 0
+
+        for row in range(mask.shape[0]):
+            for col in range(mask.shape[1]):
+                if mask[row, col] and painted_pixels < temp_pixels:
+                    prompt = torch.clone(reconstructed)
+                    prompt[:, :, row+1, col+1] = 257
+
+                    pred = net(prompt, temp=temp)
+
+                    reconstructed[:, :, row+1, col+1] = pred[:, :, row+1, col+1]
+
+                    temp = max(min_temp, temp - temp_dec_rate)
+                    painted_pixels += 1
+                elif mask[row, col]:
+                    prompt[:, :, row+1, col+1] = 257
+
+        logits = net(reconstructed)
+        pred = torch.argmax(logits, dim=4)
+
+        pred = pred[0, :, 1:, 1:]
+        pred = pred.permute(1, 2, 0)
+
+        return pred
+
+
+
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# dat = md.PixelDataset(color=True, filepath="Datasets/Cartoons/Train")
+
+device = torch.device("cpu")
+dat = md.PixelDataset(color=True, filepath="Datasets/Cartoons/Test")
+
 training_args = {
     "epochs" : 100,
     "batch_size" : 1,
     "im_rows" : 36,
     "net" : ConditionalRowRNN(embed_size=64, hidden_size=96, num_layers=5, channels=3, device=device),
-    "dat" : md.PixelDataset(color=True, filepath="Datasets/Cartoons/Train"),
+    "dat" : dat,
     "file_name" : "Models/NewInfill.pt",
     "device" : device,
     "starting_infill_pixels" : 10,
@@ -117,64 +172,42 @@ state_dict = torch.load("Models/NewInfillAdamGood.pt", map_location=torch.device
 net.load_state_dict(state_dict)
 net.eval()
 
-grid_size = 36
-infill_pixel_count = 30
-
-# Classic reconstruction. (Sanity Check)
-dat = md.PixelDataset(filepath="Datasets/Cartoons/Test", color=True)
 with torch.no_grad():
     for im in dat:
-        reconstructed = torch.clone(im.view(1, 3, grid_size+1, grid_size+1))
-        # for _ in range(infill_pixel_count):
-        #     rand_row = np.random.randint(0,grid_size)
-        #     rand_col = np.random.randint(0,grid_size)
-        #
-        #     obstructed[:, :, rand_row+1:rand_row+2, rand_col+1:rand_col+2] = 257
+        obstructed = torch.clone(im.view(1, 3, 37, 37))
+        display_im = im.permute(1, 2, 0).numpy()[1:,1:,:]
+        unchanged = display_im.copy()
 
         mask = np.full((36,36), fill_value=False)
-        mask[15:20, 15:20] = True
-        # mask[3:, 23:] = True
-        it = 0
-        limit = 10000
-        cur_temp = 0.8
+        mask[15:20, 15:25] = True
 
-        for row in range(36):
-            for col in range(36):
-                if mask[row, col] and it < limit:
-                    prompt = torch.clone(reconstructed)
-                    prompt[:, :, row+1, col+1] = 257
+        # pred = perform_infill_with_temperature(net, im, mask, start_temp=1.0, temp_pixels=20)
+        for c in range(3):
+            obstructed[0, c][1:,1:][mask] = 257
+        display_im[mask] = np.array([0, 255, 0])
 
-                    # logits = net(prompt)
-                    # pred = torch.argmax(logits, dim=4)
-
-                    pred = net(prompt, temp=cur_temp)
-
-                    reconstructed[:, :, row+1, col+1] = pred[:, :, row+1, col+1]
-
-                    cur_temp = max(0.1, cur_temp - 0.05)
-                    it += 1
-                elif mask[row, col]:
-                    reconstructed[:, :, row+1, col+1] = 257
-
-        # pred = reconstructed[0, :, 1:, 1:]
-        # obstructed[:,:,15:25,15:25] = 257
-
-        # Convert logits to an image
-        logits = net(reconstructed)
+        # Convert logits to an image - generate prediction
+        logits = net(obstructed)
         pred = torch.argmax(logits, dim=4)
         pred = pred[0, :, 1:, 1:]
-
-        im = im.view(1, 3, grid_size + 1, grid_size + 1)[0, :, 1:, 1:]
         pred = pred.permute(1, 2, 0)
-        im = im.permute(1, 2, 0).clamp(0, 255)
 
-        fig, ax = plt.subplots(1, 2)
-        ax[0].imshow(im)
-        ax[1].imshow(pred)
+        fig, ax = plt.subplots(2, 2)
 
-        # # Generate some samples to look at
-        # for s in range(4):
-        #     samp = generate_with_temperature(net, obstructed, 2)
-        #     ax[1, s].imshow(samp[:, 1:, :])
+        for i, j in np.ndindex((2,2)):
+            ax[i, j].axis('off')
+
+        # Display the unchanged and masked images
+        ax[0, 0].imshow(unchanged)
+        ax[0, 0].set_title("Original")
+        ax[0, 1].imshow(display_im)
+        ax[0, 1].set_title("Original w/ Mask")
+
+        # Display a zero temperature and slightly warm prediction
+        ax[1, 0].imshow(pred)
+        ax[1, 0].set_title("Without Temperature")
+        temp_pixels = 10
+        ax[1, 1].imshow(perform_infill_with_temperature(net, obstructed, mask, temp_pixels=temp_pixels, start_temp=1))
+        ax[1, 1].set_title(f"With {temp_pixels} Temperature Steps")
 
         plt.show()
